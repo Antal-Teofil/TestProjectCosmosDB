c3421636a26f118b4ad78883325cfd43ef0343d1
Antal-Teofil <antalteofil.githubclient@gmail.com>
2025-08-11 10:17:33 +0300

    Basic Connection

---
+++ TestCosmosDBProjectSolution/TestConnectionCosmos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using Microsoft.Azure.Cosmos;
+using Container = Microsoft.Azure.Cosmos.Container;
+
+namespace TestConnectionCosmos;
+class Program
+{
+    static async Task Main()
+    {
+        string endpointUri = "https://localhost:8081/";
+        string primaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+        CosmosClient client = new CosmosClient(endpointUri, primaryKey);
+
+        Database db = await client.CreateDatabaseIfNotExistsAsync("TestDB");
+        Container container = await db.CreateContainerIfNotExistsAsync("TestContainer", "/id");
+
+        Console.WriteLine("Connected to Cosmos DB Emulator.");
+    }
+}
